666f1e493b4fbbd2ffbb41bc560bc7aaaf308817
Jari Pennanen <pennasenjari@gmail.com>
2024-09-19 11:04:52 +0300

    Add assignment 6.18

---
+++ lecture-6/assignment-6.18/area-of-square.js
@@ -0,0 +1,12 @@
+/* Create a variable for the length of a side in a square.
+Calculate and print out the area of a square with the given length of each side.
+For example, a square with sides that are 5 meters long is 25 square meters in area.
+Extra: Calculate the area with expnonentiation ** instead of multiplication *. */
+
+const sideLen = 5; // m
+let area = sideLen * sideLen;
+console.log(`Square's side is ${sideLen} m and area is ${area} m2.` );
+
+// extra
+area = sideLen ** 2;
+console.log(`The area calculated with exponentiation is also ${area} m2.` );
