e700fb495f2abe99ef998f61564ec60268ad0eac
Jari Pennanen <pennasenjari@gmail.com>
2024-09-19 11:23:03 +0300

    Add assignment 6.20

---
+++ lecture-6/assignment-6.20/comparison.js
@@ -0,0 +1,20 @@
+/* When you have two numbers, you can compare which is greater 
+by using the > (greater than) operator:
+
+const person1Age = 15;
+const person2Age = 24;
+
+const isFirstPersonOlder = person1Age > person2Age;
+console.log(isFirstPersonOlder);
+What does the code above print? Make an educated guess, 
+then run the code and see if you were correct.
+What is the type of isFirstPersonOlder? */
+
+// Will print false
+
+const person1Age = 15;
+const person2Age = 24;
+
+const isFirstPersonOlder = person1Age > person2Age;
+console.log(isFirstPersonOlder);
+console.log(typeof(isFirstPersonOlder)); // boolean
