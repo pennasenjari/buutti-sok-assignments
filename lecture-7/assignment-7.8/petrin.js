66fa03b71c591356f7afdbdfc8843711a332b009
Jari Pennanen <pennasenjari@gmail.com>
2024-09-24 11:40:14 +0300

    Add example functions to assignment 7.8

---
+++ lecture-7/assignment-7.8/petrin.js
@@ -0,0 +1,5 @@
+[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].forEach(
+  function (days, i){
+      console.log(`Month ${i+1}: ${days} days`)
+  }
+);
---
+++ lecture-7/assignment-7.8/villen.js
@@ -0,0 +1,2 @@
+[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].forEach((days, i) => console.log(`Month ${i+1}: ${days} days`));
+
