65c4bf18f7f7dbc7a8d343ddd65207003d9f8457
Jari Pennanen <pennasenjari@gmail.com>
2024-09-30 11:12:16 +0300

    Add assignment 9.23

---
+++ lecture-9/assignment-9.23/reversed-alphabet.js
@@ -0,0 +1,36 @@
+/* const charIndex = {
+  a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9,
+  j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17,
+  r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+};
+
+Create a function charIndexString that turns any given string into a 
+  charIndex version of the string.
+
+Examples:
+
+console.log(charIndexString("bead")); // prints "2514"
+console.log(charIndexString("rose")); // prints "1815195"
+
+Tip: go through the string letter-by-letter. For each letter, 
+  figure out its charIndex. Then convert the charIndex into a string and
+  add it to the string that you later return. */
+
+
+const charIndex = {
+  a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9,
+  j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17,
+  r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+};
+  
+function charIndexString(str) {
+  const charArr = str.split("");
+  let output = "";
+  for(i = 0; i < charArr.length; i++) {
+    output += charIndex[charArr[i]];
+  }
+  return output;
+}
+  
+console.log(charIndexString("bead")); // prints "2514"
+console.log(charIndexString("rose")); // prints "1815195"
