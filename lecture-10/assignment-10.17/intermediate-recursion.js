e7af98d8dfb154efcc426033c3a7bd7d801b5668
Jari Pennanen <pennasenjari@gmail.com>
2024-10-02 20:16:28 +0300

    Refactor assignment 10.17

--- lecture-10/assignment-10.17/intermediate-recursion.js
+++ lecture-10/assignment-10.17/intermediate-recursion.js
@@ -13,6 +13,7 @@ console.log(sentencify(wordArray, 1)); // prints "quick silver wolf!" */
 
 const wordArray = [ "The", "quick", "silver", "wolf" ];
 
+// Non-recursive version
 function sentencify(wordArray, startIndex) {
   let ret = "";
   wordArray.map((word, mapIndex) => {
@@ -27,6 +28,29 @@ function sentencify(wordArray, startIndex) {
   return ret;
 }
 
-console.log("");
+console.log(`\nSentencify without recursion:`);
 console.log(sentencify(wordArray, 0)); // prints "The quick silver wolf!"
 console.log(sentencify(wordArray, 1)); // prints "quick silver wolf!" */
+
+// Recursive version
+let ret = "";
+function sentencify2(wordArray, startIndex, recur = false) {
+
+  if (recur === false) {
+    // clear the return value on 1st call
+    ret = "";
+  }
+
+  if (startIndex < wordArray.length - 1) {
+    ret += wordArray[startIndex] + " ";
+    startIndex++;
+    sentencify2(wordArray, startIndex, true); // true = recursion
+  } else {
+    ret += wordArray[startIndex] + "!";
+  }
+  return ret;
+}
+
+console.log(`\nSentencify with recursion:`);
+console.log(sentencify2(wordArray, 0)); // prints "The quick silver wolf!"
+console.log(sentencify2(wordArray, 1)); // prints "quick silver wolf!" */
