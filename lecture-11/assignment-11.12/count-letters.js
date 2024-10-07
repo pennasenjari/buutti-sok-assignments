37d65251875573a4a2f15188cbee131cb4007aaa
Jari Pennanen <pennasenjari@gmail.com>
2024-10-07 10:32:54 +0300

    Refactor assignment 11.12 to print results alphabetically

--- lecture-11/assignment-11.12/count-letters.js
+++ lecture-11/assignment-11.12/count-letters.js
@@ -20,38 +20,34 @@ console.log(result);
 }
 */
 
-/* charIndex not needed!
 const charIndex = {
   a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9,
   j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17,
   r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
 };
-*/
 
 function getCountOfLetters(str) {
   const charArr = str.split(""); // convert string into character array
-  let charList = {}; // the result
-  for(char of charArr) { // loop each letter in str
-    char = char.toLowerCase(); // case insensitive counting
-    if (char != " ") { // omit empty spaces
-      if (!(char in charList)) {
-        charList[char] = 1; // init object property (current letter) and set value to 1
-      } else {
-        charList[char]++; // increase letter count
+  let charObj = {}; // the result
+  // loop charIndex to sort result alphabetically
+  for(char of Object.keys(charIndex)) {
+    for (i = 0; i < charArr.length; i++) { // loop given string
+      if (charArr[i] != " ") { // omit empty spaces
+        charArr[i] = charArr[i].toLowerCase(); // case insensitive
+        if (char === charArr[i]) {
+          if (!(charArr[i] in charObj)) {
+            /* The property (letter) doesn't exist yet. 
+               Create and set value to 1. */
+            charObj[char] = 1; 
+          } else {
+            charObj[char]++; // increase letter count
+          }
+        }
       }
     }
   }
-  return charList;
+  return charObj;
 }
 
 const result = getCountOfLetters("A black cat");
-console.log(result); 
-/* prints 
-{
-    a: 3,
-    b: 1,
-    c: 2,
-    k: 1,
-    l: 1,
-    t: 1
-} */
+console.log(result);  // { a: 3, b: 1, c: 2, k: 1, l: 1, t: 1 }
