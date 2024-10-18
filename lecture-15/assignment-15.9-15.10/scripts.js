91421e72cb87c5b056b87ec9314bc2baaac042fb
Jari Pennanen <pennasenjari@gmail.com>
2024-10-18 13:06:22 +0300

    Refine assignment 15.9-15.10

--- lecture-15/assignment-15.9-15.10/scripts.js
+++ lecture-15/assignment-15.9-15.10/scripts.js
@@ -3,17 +3,24 @@ let r = 0;
 let g = 255;
 let b = 0;
 let w = 90; // vw
-const stepPercent = 10;
-/* Note: color has to diminish faster than size to see completely red bar before it 
-disappears! For example, the if the bar disappears in 10 clicks, the bar needs to be
+const steps = 15;
+/* Note: color has to diminish faster than width to see completely red bar before it 
+disappears! For example, if the bar disappears in 10 clicks, the bar needs to be
 completely red at click 9. */
-// rounding numbers causes small throw in color, but close enough
-const colorStep = (parseInt(g / (stepPercent - 1))); 
-const widthStep = (parseInt((w / stepPercent)));
+// rounding the numbers causes small throw in color
+const colorStep = (Math.round(g / (steps - 1))); 
+const widthStep = (Math.round((w / steps)));
 slider.addEventListener("click", function() {
   r += colorStep;
   g -= colorStep;
   w -= widthStep;
+  /* Correct the rounding problem at last step to set
+     the last color to plain red */
+  if (w <= widthStep) {
+    r = 255;
+    g = 0;
+  }
+  //console.log(r, g, b);
   this.style.width = `${w}vw`;
   this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 })
